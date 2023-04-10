8f8e5713ca641a964ab1b1e858f704851386472f
unknown <shivamsparekh05@gmail.com>
2023-04-09 20:52:53 -0400

    changed url

--- signupbackend/server.js
+++ signupbackend/server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 // Add CORS headers using the cors middleware
 app.use(cors({
-  origin: 'https://kitchener-youths-8iwoqyqv8-shivamp1771.vercel.app',
+  origin: 'https://yds-eight.vercel.app',
   credentials: true
 }))
 
